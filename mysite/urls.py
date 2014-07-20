7564b96808afd4f7854326d8c2504e6fc1fac648
yatintaluja <yatintaluja@gmail.com>
2014-07-20 16:23:32 +0530

    updated

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,6 @@
-from django.conf.urls import patterns, include, url
+
+from django.conf.urls import *
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 
